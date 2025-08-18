4616c6268d61184985907cd4e2ac23e1a51fee37
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-08-18 08:03:07 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,2 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_malmo-user
-add_lunch_combo omni_malmo-userdebug
-add_lunch_combo omni_malmo-eng
+[
+]
